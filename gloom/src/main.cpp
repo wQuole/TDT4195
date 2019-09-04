648ccfd3f3376f2426958d50646d924e0cbfbb7a
William Kvaale <wgkvaale@gmail.com>
2019-09-05 00:02:19 +0200

    Add setUpVAOTriangle function

--- gloom/src/main.cpp
+++ gloom/src/main.cpp
@@ -28,7 +28,7 @@ GLFWwindow* initialise()
 
     // Set core window options (adjust version numbers if needed)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Enable the GLFW runtime error callback function defined previously.
