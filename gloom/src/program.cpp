5b8785f06b1b37e0953c43cf197ba20226568738
wQuole <wgkvaale@gmail.com>
2019-10-08 21:44:00 +0200

    Clean up

--- gloom/src/program.cpp
+++ gloom/src/program.cpp
@@ -10,17 +10,19 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtx/transform.hpp>
 
-
 using namespace std;
 
 unsigned int NUM_OF_VERTCOORDS = 3;
 GLfloat X_COORD = 0.0f;
 GLfloat Y_COORD = 0.0f;
-GLfloat Z_COORD = -3.0f;
+GLfloat Z_COORD = -2.0f;
 GLfloat X_ROT = 0.0f;
 GLfloat Y_ROT = -0.0f;
 
+// Declaring createVAO function
 GLuint createVAO(vector<GLfloat> vertexCoords, vector<GLuint> vertexIndices, vector<GLfloat> colors);
+
+// Defining createVAO function
 GLuint createVAO(vector<GLfloat> vertexCoords, vector<GLuint> vertexIndices, vector<GLfloat> colors)
 {
     auto vectorSizeInBytes = vertexCoords.size() * sizeof(GLfloat);
@@ -61,6 +63,20 @@ GLuint createVAO(vector<GLfloat> vertexCoords, vector<GLuint> vertexIndices, vec
     return vertexBufferID;
 }
 
+glm::mat4 rotateAndTranslate() {
+    // Create and perspective Matrices
+    glm::mat4x4 Projection = glm::perspective(glm::radians(45.0f), 4.0f/3.0f, 1.0f, 100.0f); // https://glm.g-truc.net/0.9.9/index.html
+
+    // Translation
+    glm::mat4x4 View = glm::translate(glm::mat4(1.0f), glm::vec3(X_COORD, Y_COORD, Z_COORD));
+    glm::mat4x4 ViewTransposed = glm::transpose(View);
+
+    // Rotation
+    glm::mat4x4 X_rotMatrix = glm::rotate(glm::radians(X_ROT), glm::vec3(1.0f, 0.0f, 0.0f));
+    glm::mat4x4 Y_rotMatrix = glm::rotate(glm::radians(Y_ROT), glm::vec3(0.0f, 1.0f, 0.0f));
+
+    return X_rotMatrix * Y_rotMatrix * ViewTransposed * Projection;
+}
 
 void runProgram(GLFWwindow* window)
 {
@@ -108,63 +124,13 @@ void runProgram(GLFWwindow* window)
 
     };
 
-    vector<GLfloat> fiveTriangles {
-            -0.9f, -0.3f, 0.0f,
-            -0.7f, 0.3f, 0.0f,
-            -0.5f, -0.3f, 0.0f,
-
-            -0.6f, 0.9f, 0.0f,
-            -0.3f, -0.4f, 0.0f,
-            -0.1f, 0.9f, 0.0f,
-
-            -0.2f, -0.4f, 0.0f,
-            0.0f, 0.3f, 0.0f,
-            0.2f, -0.4f, 0.0f,
-
-            0.1f, 0.9f, 0.0f,
-            0.3f, -0.4f, 0.0f,
-            0.6f, 0.9f, 0.0f,
-
-            0.5f, -0.3f, 0.0f,
-            0.7f, 0.3f, 0.0f,
-            0.9f, -0.3f, 0.0f,
-    };
-
-    vector<GLfloat> fiveTrianglesColors = {
-            0.5f, 0.0f, 0.0f, 0.7331f,
-            0.0f, 0.5f, 0.0f, 0.7331f,
-            0.0f, 0.0f, 0.5f, 0.7331f,
-
-            0.5f, 0.0f, 0.0f, 0.7331f,
-            0.0f, 0.5f, 0.0f, 0.7331f,
-            0.0f, 0.0f, 0.5f, 0.7331f,
-
-            0.5f, 0.0f, 0.0f, 0.7331f,
-            0.0f, 0.5f, 0.0f, 0.7331f,
-            0.0f, 0.0f, 0.5f, 0.7331f,
-
-            0.5f, 0.0f, 0.0f, 0.7331f,
-            0.0f, 0.5f, 0.0f, 0.7331f,
-            0.0f, 0.0f, 0.5f, 0.7331f,
-
-            0.5f, 0.0f, 0.0f, 0.7331f,
-            0.0f, 0.5f, 0.0f, 0.7331f,
-            0.0f, 0.0f, 0.5f, 0.7331f,
-    };
-
-
-    // Set up your scene here (create Vertex Array Objects, etc.)
-    // string vertPath = "/Users/wquole/CLionProjects/TDT4195/gloom/shaders/simple.vert"; // MacOS
-    // string fragPath = "/Users/wquole/CLionProjects/TDT4195/gloom/shaders/simple.frag"; // MacOS
-    string vertPath = "C:\\Users\\wquole\\code\\cppCode\\TDT4195\\gloom\\shaders\\simple.vert"; // Windows
-    string fragPath = "C:\\Users\\wquole\\code\\cppCode\\TDT4195\\gloom\\shaders\\simple.frag"; // Windows
-
+    string vertPath = R"(C:\Users\wquole\code\cppCode\TDT4195\gloom\shaders\simple.vert)";
+    string fragPath = R"(C:\Users\wquole\code\cppCode\TDT4195\gloom\shaders\simple.frag)";
 
     // Fill the indices
     vector<GLuint> triangleIndices(threeOverlappingTriangles.size());
     iota(triangleIndices.begin(), triangleIndices.end(), 0);
     GLuint numberOfVertices = (int) threeOverlappingTriangles.size() / NUM_OF_VERTCOORDS;
-
 
     // Activate shader and bind the Vertex Array
     Gloom::Shader shader;
@@ -174,9 +140,6 @@ void runProgram(GLFWwindow* window)
     GLuint arrayID = createVAO(threeOverlappingTriangles, triangleIndices, threeOverlappingTrianglesColors);
     glBindVertexArray(arrayID);
 
-    // Create identity- and perspective Matrices
-    glm::mat4x4 Projection = glm::perspective(glm::radians(45.0f), 4.0f/3.0f, 1.0f, 100.0f); // https://glm.g-truc.net/0.9.9/index.html
-
     // Rendering Loop
     printGLError();
     GLuint increment =  0;
@@ -185,18 +148,8 @@ void runProgram(GLFWwindow* window)
         // Clear colour and depth buffers
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // T R A N S F O R M A T I O N S
-        // Transposing --> ref: https://stackoverflow.com/questions/13293469/why-does-my-translation-matrix-needs-to-be-transposed
-        // Translation
-        glm::mat4x4 View = glm::translate(glm::mat4(1.0f), glm::vec3(X_COORD, Y_COORD, Z_COORD));
-        glm::mat4x4 ViewTransposed = glm::transpose(View);
-
-        // Rotation
-        glm::mat4x4 X_rotMatrix = glm::rotate(glm::radians(X_ROT), glm::vec3(1.0f, 0.0f, 0.0f));
-        glm::mat4x4 Y_rotMatrix = glm::rotate(glm::radians(Y_ROT), glm::vec3(0.0f, 1.0f, 0.0f));
-
-        // Result aka Final Matrix
-        glm::mat4x4 TransformedMatrix = X_rotMatrix * Y_rotMatrix * ViewTransposed * Projection;
+        // Transform matrix
+        glm::mat4x4 TransformedMatrix = rotateAndTranslate();
 
         // Send to Vertex Shader
         glUniformMatrix4fv(3, 1, GL_FALSE, glm::value_ptr(TransformedMatrix));
@@ -232,48 +185,48 @@ void handleKeyboardInput(GLFWwindow* window)
     }
     else if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
     {
-        X_COORD += 0.01;
+        X_COORD += 0.01f;
     }
     else if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
     {
-        X_COORD -= 0.01;
+        X_COORD -= 0.01f;
     }
     else if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
     {
-        Y_COORD -= 0.01;
+        Y_COORD -= 0.01f;
     }
     else if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
     {
-        Y_COORD += 0.01;
+        Y_COORD += 0.01f;
     }
     else if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
     {
-        Z_COORD -= 0.01;
+        Z_COORD -= 0.01f;
     }
     else if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
     {
-        Z_COORD += 0.01;
+        Z_COORD += 0.01f;
     }
     else if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
     {
-        X_ROT -= 1.0;
+        X_ROT -= 1.0f;
     }
     else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
     {
-        X_ROT += 1.0;
+        X_ROT += 1.0f;
     }
     else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
     {
-        Y_ROT += 1.0;
+        Y_ROT += 1.0f;
     }
     else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
-        Y_ROT -= 1.0;
+        Y_ROT -= 1.0f;
     }
     else if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) {
         X_ROT = 0.0f;
         Y_ROT = 0.0f;
         X_COORD = 0.0f;
         Y_COORD = 0.0f;
-        Z_COORD = -3.0f;
+        Z_COORD = -2.0f;
     }
 }
